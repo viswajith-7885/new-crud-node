405f98a79fdf0b5f08723e22a1e3f899beacdb6f
viswajith-7885 <viswajith21295@gmail.com>
2024-01-04 16:48:20 +0530

    third commit

--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,12 @@
 const express= require('express');
 const router = express.Router();
 
-router.get('/users',(req,res)=>{
+router.get('/',(req,res)=>{
     res.render('index',{title:'home page'})
+})
+
+router.get('/adduser',(req,res)=>{
+    res.render('add_users',{title:'add user'})
 })
 
 module.exports =router;
